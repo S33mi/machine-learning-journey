1a7477e9887dc1d5d9a4e281cffbe16de72acc7b
S33mi <122097166+S33mi@users.noreply.github.com>
2026-06-27 13:37:26 +0500

    Add polynomial fitting function in MATLAB

---
+++ matlab_implementations/01_polynomial_curve_fitting/fit_polynomial_ML.m
@@ -0,0 +1,15 @@
+function w = fit_polynomial_ML(X, T, M)
+    % X: input vector (N x 1)
+    % T: target vector (N x 1)
+    % M: polynomial degree
+    
+    N = length(X);
+    Phi = zeros(N, M+1);  % Design matrix
+    
+    for j = 0:M
+        Phi(:, j+1) = X .^ j;
+    end
+    
+    % ML solution: w = (Phi' * Phi)^-1 * Phi' * T
+    w = Phi \ T;   % More stable than inv(Phi'*Phi)*Phi'*T
+end
